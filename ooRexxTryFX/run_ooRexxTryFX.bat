4c0cf1be106203de79c781837dcbd5115fe6e3db
Adrian Baginski <abaginski@live.de>
2017-07-07 20:12:23 +0200

    working exe

---
+++ ooRexxTryFX/run_ooRexxTryFX.bat
@@ -0,0 +1,33 @@
+@setlocal
+@echo off
+rem this batch file was converted to exe using the application "Bat To Exe Converter v2.4.3" by <www.f2ko.de>
+rem change working directory to that of ooRexxTryFX
+cd %~dp0
+rem set the title of the cmd window
+title ooRexxTryFX
+rem call the concat function for all arguments 
+for %%i in (%*) do (
+  if not "%0"=="%%i" ( 
+    call :concat %%i
+  )
+)
+rem search properties file for showConsole property
+rem show the console if the porperty is set to .true aka 1
+%extd% /readpreferencevalue resources/config/preferences.conf showConsole
+rem extd is a function provided by the bat to exe converter
+if %result%==1 %extd% /showself
+
+set dependencies=richtextfx-0.7-M5-custom ikonli-openiconic-pack-1.9.0 ikonli-octicons-pack-1.9.0 ikonli-javafx-1.9.0 ikonli-core-1.9.0
+rem add each dependency to the classpath via the addToPath function
+for %%a in (%dependencies%) do call :addToPath %%a
+rem start ooRexxTryFX with all of its arguments
+rexx %cd%\ooRexxTryFX.rxj%args%
+goto :eof
+
+:addToPath
+set CLASSPATH=%cd%\%1.jar;%CLASSPATH%
+goto :eof
+
+:concat
+set args=%args% %1
+goto :eof
